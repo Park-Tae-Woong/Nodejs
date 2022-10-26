f864a7d57e16981cf2b50f628699f3890d655f22
박태웅 <107820746+Park-Tae-Woong@users.noreply.github.com>
2022-10-27 03:16:07 +0900

    노드 공부

---
+++ syntax/conditional.js
@@ -0,0 +1,7 @@
+var args = process.argv;
+console.log(args[2]);
+if (args[2]=='1'){
+  console.log('c1');
+} else {
+  console.log('c2');
+}
---
+++ syntax/template.js
@@ -0,0 +1,8 @@
+var name = "woong";
+var letter = 'Lorem ' + name + 'ipsum dolor sit amet, \
+consectetur adipisicing elit, sed do eiusmod tempor \
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+var letter = `Lorem ${name} ipsum dolor sit amet, conectetur adipisicing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+console.log(letter);
+console.log(letter);
